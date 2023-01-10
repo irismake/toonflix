cf53d09f7584b591f1e5e3529146c162aed41216
irismake <iris3455@gmail.com>
2023-01-10 21:15:25 +0900

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    );
+  }
+}
