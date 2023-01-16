012266ecdb85884a82896a6eb48175fedcb798c1
Iris <iris3455@gmail.com>
2023-01-16 16:16:54 +0900

    Style : ListView.seperated
    Show data with ListView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:toonflix/services/api_service.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -25,11 +25,24 @@ class HomeScreen extends StatelessWidget {
       ),
       body: FutureBuilder(
         future: webtoons,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return const Text('There is data!');
+        builder: (context, futureResult) {
+          if (futureResult.hasData) {
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: futureResult.data!.length,
+              itemBuilder: (context, index) {
+                print(index);
+                var webtoon = futureResult.data![index];
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                width: 20,
+              ),
+            );
           }
-          return const Text('Loading');
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
       ),
     );
